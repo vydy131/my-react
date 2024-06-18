b1995ba0ba529b8b1a8608305c85960b6c0516a4
vydy131 <vydy131@gmail.com>
2024-06-18 20:03:16 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
   // )
 
 
-  
+  const [count, setCount] = useState(0)
 
   return React.createElement('div', {className : 'container'}, 
-    React.createElement('h1', {className: 'font-bold', key:0}, 'Test JSX'),
-    React.createElement('button', {className : 'py-2, px-4 border', key:1, onClick : () => console.log('Clicked!')}, 'click me')
+    React.createElement('h1', {className: 'font-bold', key:0}, `Test JSX ${count}`),
+    React.createElement('button', {className : 'py-2, px-4 border', key:1, onClick : () => setCount(count + 1)}, 'click me')
   )
 }
 

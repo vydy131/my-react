2fe1cc6a57c050806aaf82d8cd774f000dc91a0d
vydy131 <vydy131@gmail.com>
2024-06-18 19:43:35 +0300

    hello world website

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+  //   <h1>Hello React</h1>
+  // )
+
+
+
+
+  return React.createElement('div', {className : 'container'}, 
+    React.createElement('h1', {className: 'font-bold'}, 'Test JSX'),
+    React.createElement('button', {className : ''}, 'click me')
+  )
 }
 
 export default App;

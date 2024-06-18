3ed9da26d9817a8416d87df841c23fc819974f47
vydy131 <vydy131@gmail.com>
2024-06-18 21:31:35 +0300

    data output

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Product } from './components/Product'
+import { products } from './data/fakedata';
 
 function App() {
   // return (
@@ -14,7 +15,8 @@ function App() {
 
   return(
     <div className = "container mx-auto max-w-2xl pt-5">
-      <Product/>
+      <Product product = {products[0]}/>
+      <Product product = {products[1]}/>
     </div>
   )
 }
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,18 @@
-export function Product (){
+import { IProduct } from "../models"
+
+interface ProductProps {
+    product : IProduct
+}
+
+export function Product (props: ProductProps){
+    console.log(props)
     return (
-        <div>
-            Product!
+        <div
+        className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img src = {props.product.image} className="w-1/6" alt={props.product.title}/>
+            {props.product.title}
+            <span className="font-bold">{props.product.price}</span>
+            <p>{props.product.description}</p>
         </div>
     )
 }

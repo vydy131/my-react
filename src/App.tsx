c4605bd91cd69f5b61bc8f4cd8dd172367d1ef00
vydy131 <vydy131@gmail.com>
2024-06-18 21:02:31 +0300

    components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { Product } from './components/Product'
 
 function App() {
   // return (
   //   <h1>Hello React</h1>
   // )
+  // ##############
+  // const [count, setCount] = useState(0)
+  // return React.createElement('div', {className : 'container'}, 
+  //   React.createElement('h1', {className: 'font-bold', key:0}, `Test JSX ${count}`),
+  //   React.createElement('button', {className : 'py-2, px-4 border', key:1, onClick : () => setCount(count + 1)}, 'click me')
+  // )
 
-
-  const [count, setCount] = useState(0)
-
-  return React.createElement('div', {className : 'container'}, 
-    React.createElement('h1', {className: 'font-bold', key:0}, `Test JSX ${count}`),
-    React.createElement('button', {className : 'py-2, px-4 border', key:1, onClick : () => setCount(count + 1)}, 'click me')
+  return(
+    <div className = "container mx-auto max-w-2xl pt-5">
+      <Product/>
+    </div>
   )
 }
 
---
+++ src/components/Product.tsx
@@ -0,0 +1,7 @@
+export function Product (){
+    return (
+        <div>
+            Product!
+        </div>
+    )
+}

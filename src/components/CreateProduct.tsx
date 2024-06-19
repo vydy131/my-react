a64b7da12446ee8f3e24fd14e0e147ffd221a897
vydy131 <vydy131@gmail.com>
2024-06-19 13:44:05 +0300

    mistake solved

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,16 +1,42 @@
 import React, { useState } from "react"
+import { IProduct } from "../models"
+import axios from "axios"
+import { ErrorMessage } from "./ErrorMessage"
+
+const productData : IProduct = {
+    title: 'test product',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating : {
+        rate : 42,
+        count: 10 
+    }
+}
 
 export function CreateProduct(){
 
     const [value, setValue] = useState('')
+    const [error, setError] = useState('')
 
-    const submitHandler = (event : React.FormEvent) => {
+    const submitHandler = async (event : React.FormEvent) => {
         event.preventDefault()
+        setError('')
+
+        if (value.trim().length === 0){
+            setError('Please enter valid title')
+            return
+        }
+
+        productData.title = value
+
+        const respones = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
     }
 
-    const changeHandler = (event : React.KeyboardEvent<HTMLInputElement>) => {
-        setValue(event.target.value)
-    }
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+      };
 
     return(
         <form onSubmit={submitHandler}>
@@ -20,6 +46,8 @@ export function CreateProduct(){
             value={value}
             onChange={changeHandler}
             />
+
+            {error && <ErrorMessage error = {error}/>}
         
             <button type='submit'
             className="py-2 px-4 border

6d888e51535118dc9a4b1056e5c10e8236537a0c
vydy131 <vydy131@gmail.com>
2024-06-19 18:03:46 +0300

    some info

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header (){
+    return(
+      <header>
+        <h3>
+          Logo or name
+        </h3>
+        <span>
+          Contacts & help
+        </span>
+      </header>
+    )
+  }

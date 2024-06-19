9debbd955c9d9abebba8ed93903939567da08290
vydy131 <vydy131@gmail.com>
2024-06-19 14:54:13 +0300

    mistake with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,15 @@
-import { useContext } from 'react';
-import { CreateProduct } from './components/CreateProduct';
-import { ErrorMessage } from './components/ErrorMessage';
-import { Loader } from './components/Loader';
-import { Modal } from './components/Modal';
-import { Product } from './components/Product'
-import { useProducts } from './hooks/products';
-import { IProduct } from './models';
-import { ModalContext } from './context/ModalContext';
+import {Route,  Routes } from "react-router-dom";
+import { ProductsPage } from "./pages/ProductsPage";
+import { AboutPage } from "./pages/AboutPage";
 
 
 function App() {
 
-  const {modal, open, close} = useContext(ModalContext)
-
-  const {loading, error, products, addProduct} = useProducts()
-
-  const createHandler = (product : IProduct) => {
-    close()
-    addProduct(product)
-  }
-
-  return(
-    <div className = "container mx-auto max-w-2xl pt-5">
-      {loading && <Loader/>}
-      {error && <ErrorMessage error = {error}/>}
-      {products.map(product => <Product product={product} key={product.id}/>)}
-      {modal &&
-      <Modal title='Create new product' onClose={close}>
-        <CreateProduct onCreate={createHandler}/>
-      </Modal>}
-
-      <button className="fixed bottom-5 right-5
-      rounded-full bg-red-700 
-      text-white text-2xl px-4 py-2"
-      onClick={open}>+</button>
-    </div>
-  )
+  <Routes>
+    <Route path="/" element={<ProductsPage/>}/>
+    <Route path="/about" element={<AboutPage/>}/>
+  </Routes>
+  
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ModalState } from './context/ModalContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <BrowserRouter>
     <ModalState>
-    <App />
+      <App />
     </ModalState>
+  </BrowserRouter>
+    
 );
 

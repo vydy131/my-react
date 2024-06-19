716e5070990f8bde0fa42feef48d87f7e000ae98
vydy131 <vydy131@gmail.com>
2024-06-19 11:59:33 +0300

    modal interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { CreateProduct } from './components/CreateProduct';
 import { ErrorMessage } from './components/ErrorMessage';
 import { Loader } from './components/Loader';
+import { Modal } from './components/Modal';
 import { Product } from './components/Product'
 import { useProducts } from './hooks/products';
 
@@ -13,6 +15,10 @@ function App() {
       {loading && <Loader/>}
       {error && <ErrorMessage error = {error}/>}
       {products.map(product => <Product product={product} key={product.id}/>)}
+
+      <Modal>
+        <CreateProduct/>
+      </Modal>
     </div>
   )
 }

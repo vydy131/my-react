f775c931ce1a2e2a01ac99e95bfb2be790b5fa92
vydy131 <vydy131@gmail.com>
2024-06-19 11:38:45 +0300

    components files

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import { ErrorMessage } from './components/ErrorMessage';
+import { Loader } from './components/Loader';
 import { Product } from './components/Product'
-// import { products } from './data/fakedata';
-import axios, { AxiosError } from 'axios';
-import { IProduct } from './models';
+import { useProducts } from './hooks/products';
+
 
 function App() {
-  // return (
-  //   <h1>Hello React</h1>
-  // )
-  // ##############
-  // const [count, setCount] = useState(0)
-  // return React.createElement('div', {className : 'container'}, 
-  //   React.createElement('h1', {className: 'font-bold', key:0}, `Test JSX ${count}`),
-  //   React.createElement('button', {className : 'py-2, px-4 border', key:1, onClick : () => setCount(count + 1)}, 'click me')
-  // )
 
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  async function fetchProducts (){
-    try {
-      setError('')
-      setLoading(true)
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-      setProducts(response.data)
-      setLoading(false)
-    }
-    catch (e : unknown){
-      const error = e as AxiosError
-      setLoading(false)
-      setError(error.message)
-    }
-    
-  }
-
-  useEffect(() => {fetchProducts()}, [])
+  const {loading, error, products} = useProducts()
 
   return(
     <div className = "container mx-auto max-w-2xl pt-5">
-      {loading && <p className='text-center'>Loading...</p>}
-      {error && <p className='text-center text-red-600'>{error}</p>}
+      {loading && <Loader/>}
+      {error && <ErrorMessage error = {error}/>}
       {products.map(product => <Product product={product} key={product.id}/>)}
-      
-      {/* <Product product = {products[0]}/>
-      <Product product = {products[1]}/> */}
     </div>
   )
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export function Loader(){
+    return(
+        <div>  
+            <p className='text-center'>Loading...</p>
+        </div>
+    )
+}

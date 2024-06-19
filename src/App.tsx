00193d3e5355ba4508280b63e2e37bf3f3d5d750
vydy131 <vydy131@gmail.com>
2024-06-19 11:18:23 +0300

    load states

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product'
-import { products } from './data/fakedata';
+// import { products } from './data/fakedata';
+import axios, { AxiosError } from 'axios';
+import { IProduct } from './models';
 
 function App() {
   // return (
@@ -13,8 +15,32 @@ function App() {
   //   React.createElement('button', {className : 'py-2, px-4 border', key:1, onClick : () => setCount(count + 1)}, 'click me')
   // )
 
+  const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+
+  async function fetchProducts (){
+    try {
+      setError('')
+      setLoading(true)
+      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+      setProducts(response.data)
+      setLoading(false)
+    }
+    catch (e : unknown){
+      const error = e as AxiosError
+      setLoading(false)
+      setError(error.message)
+    }
+    
+  }
+
+  useEffect(() => {fetchProducts()}, [])
+
   return(
     <div className = "container mx-auto max-w-2xl pt-5">
+      {loading && <p className='text-center'>Loading...</p>}
+      {error && <p className='text-center text-red-600'>{error}</p>}
       {products.map(product => <Product product={product} key={product.id}/>)}
       
       {/* <Product product = {products[0]}/>
